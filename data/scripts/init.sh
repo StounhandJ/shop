7f9899f837821feebb1f832e4b0d3d9a30ea6b44
StounhandJ <rmarinicev@gmail.com>
2021-09-27 09:24:47 +0300

    fix script to starting product version

--- data/scripts/init.sh
+++ data/scripts/init.sh
@@ -6,6 +6,6 @@ php artisan db:seed
 php artisan storage:link
 chown www-data:www-data -R /var/www/storage/app
 
-if (($PRODUCT == True)); then
+if (("$PRODUCT" = True)); then
     php artisan optimize
 fi
